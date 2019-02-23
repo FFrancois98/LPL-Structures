fd2c9f90dd0d34c502a87208b78b5e9c7a76be22
Fleurevca Francois UbuntuWin <fleurevca.francois@bison.howard.edu>
2019-02-22 21:59:17 -0500

    Upon testing my code, the orginal function didn't pass the test case where the entire list was composed of chars only. To fix this problem, I added a predicate called list_chars which would return the number of non-numbers elements in the list. I then created another recursive case for the sum-simple function that returns the value 0 when the length of the list is equal to the return value of list_chars which symbolizes that the list is entirely created of non-numbers elements. After adding this code to the program, I ran all the test cases provided and was able to pass them all.

--- prolog.pl
+++ prolog.pl
@@ -1,3 +1,12 @@
+list_chars([], 0).
+list_chars([X|Xs], N) :-
+    (   number(X)
+    ->  list_chars(Xs, N)
+    ;   list_chars(Xs, N0),
+        succ(N0, N)
+    ).
+
+
 sum-up-numbers-simple([], 0).
 
 sum-up-numbers-simple([L | LRest], Result) :-
@@ -8,3 +17,8 @@ sum-up-numbers-simple([L | LRest], Result) :-
 sum-up-numbers-simple([L | LRest], LRestResult) :-
 	not(number(L)),
 	sum-up-numbers-simple(LRest, LRestResult).
+
+sum-up-numbers-simple([L | LRest], 0) :-
+        length([L | LRest], N),
+	list_chars([L | LRest], M),
+	M == N.
